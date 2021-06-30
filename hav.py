f8a377cdc198ed44a65aefff3e3bc13f8ad5724c
pacobaco <jwookie2@hotmail.com>
2021-06-29 20:01:38 -0400

    Update hav.py
    updated to python 3

--- hav.py
+++ hav.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
-import urllib2
-import urlparse
+import urllib
+import urllib.parse
 import sys
 import string
 #from nltk import word_tokenize
 #from nltk.corpus import stopwords
-reload(sys)
-sys.setdefaultencoding('utf8')
-rr = open('rr2')
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+rr = open('rr')
 rr = rr.read()
 rr = rr.split(',')
 u = []
@@ -18,25 +18,29 @@ d3 = d2.split('\n')
 o = {}
 g = 0
 dv = {}
+headlines = {}
 #stop = set(stopwords.words('english'))
 for x in d3:
 #       try:
         try:
-                r = urllib2.urlopen(x, timeout=30).read()
+                from urllib.request import urlopen
+                r = urlopen(x,timeout=30).read()
+                #print(r)
+                #r = urllib2.urlopen(x, timeout=30).read()
         except:
                 continue
         soup = BeautifulSoup(r, "html.parser")
         if x:
-                e = urlparse.urlparse(x)
+                e = urllib.parse.urlparse(x)
         else:
                 continue
         for link in soup.findAll('a'):
-                print link
+                print(link)
                 if link!=None:
                         v = link.text
                         y = link.get('href')
                         if y:
-                                t = urlparse.urlparse(y)
+                                t = urllib.parse.urlparse(y)
                         else:
                                 continue
                         if t[0]=='':
@@ -44,6 +48,68 @@ for x in d3:
                                     y = e[0]+'://'+e[1]+y
                                 except: continue
                         v = v.rstrip(' \n\t\0')
+                        v = v.lstrip(' \n\t\0')
+                        mnb = v.split(' ')
+                        for zz in mnb:
+                                if y in headlines:
+                                        headlines[y].append(zz)
+                                else:
+                                        headlines[y] = []
+                                        headlines[y].append(zz)
+                        print(mnb, y)
+                        for x in rr:
+                                if v.find(x)>0:
+                                        if v in o==False:
+                                                o[v] = y
+                                        for e in v.lower().split():
+                                                #print e
+ #                                               if e not in stop:
+                                                       #print e
+                                            if e in dv:
+                                                dv[e] = dv[e]+1
+                                            else:
+                                                dv[e] = 1
+r = ''
+s = open('rr8', 'w')
+for x in o.keys():
+        r = r+'<a href="'+o[x]+'">'+x+'</a><br>'
+s.write(r)
+s.close()
+print(headlines)
+b=list(headlines.keys())
+b.sort()
+s = {}
+leni = {}
+for x in b:
+        for y in headlines[x]:
+                if y in s:
+                        s[y].append(x)
+                else:
+                        s[y] = []
+                        s[y].append(x)
+                if y in leni:
+                        leni[y]=leni[y]+1
+                else:
+                        leni[y]=1
+
+print(s)
+print(leni)
+b=list(leni.keys())
+b.sort()
+mmm={}
+for x in b:
+        if leni[x] in mmm:
+                mmm[leni[x]].append(x)
+        else:
+                mmm[leni[x]] = []
+                mmm[leni[x]].append(x)
+#print(b)
+b=list(mmm.keys())
+b.sort()
+for x in b:
+        print(x, mmm[x])
+print(b)
+
                         v = v.lstrip(' \n\t\0')
                         for x in rr:
                                 if string.find(v, x)>0:
